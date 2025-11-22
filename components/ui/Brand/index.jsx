18e6696a39671899b3181bbedadd94c19216c4ff
Aditya <aditya@Adityas-MacBook-Air.local>
2025-11-22 15:57:14 +0530

    Atlasforge

---
+++ components/ui/Brand/index.jsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+const Brand = () => (
+    <Link href="/">
+        <img
+            src="/atlas-forge.svg"
+            width={120}
+            height={50}
+            alt="Atlas Forge logo"
+        />
+    </Link>
+)
+export default Brand
